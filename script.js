bdcfd87922513377e771f9a960073893686a2485
NeerajaGurram <152693538+NeerajaGurram@users.noreply.github.com>
2024-02-04 17:40:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ var countDownDate = new Date("Feb 10, 2024 00:00:00").getTime();
                 blocks.forEach(block => {
                     const blockTop = block.getBoundingClientRect().top;
                     const screenHeight = window.innerHeight;
-                    if (blockTop < screenHeight/1.5) {
+                    if (blockTop < screenHeight/1.3) {
                         block.style.opacity = '1';
                         block.style.transform = 'translateX(0)';
       }
@@ -46,9 +46,49 @@ document.addEventListener('DOMContentLoaded', function () {
         bigbs.forEach(bigb => {
             const bigbTop = bigb.getBoundingClientRect().top;
             const screenHeight = window.innerHeight;
-            if (bigbTop < screenHeight/1.5) {
+            if (bigbTop < screenHeight/1.3) {
                 bigb.style.opacity = '1';
                 bigb.style.transform = 'translateX(0)';
+}
+});
+}
+
+// Initial check when the page loads
+checkScroll();
+
+// Listen for scroll events
+window.addEventListener('scroll', checkScroll);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const block1s = document.querySelectorAll('.block1');
+    function checkScroll() {
+        block1s.forEach(block1 => {
+            const block1Top = block1.getBoundingClientRect().top;
+            const screenHeight = window.innerHeight;
+            if (block1Top < screenHeight/1.3) {
+                block1.style.opacity = '1';
+                block1.style.transform = 'translateX(0)';
+}
+});
+}
+
+// Initial check when the page loads
+checkScroll();
+
+// Listen for scroll events
+window.addEventListener('scroll', checkScroll);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const bigb1s = document.querySelectorAll('.bigb1');
+    function checkScroll() {
+        bigb1s.forEach(bigb1 => {
+            const bigb1Top = bigb1.getBoundingClientRect().top;
+            const screenHeight = window.innerHeight;
+            if (bigb1Top < screenHeight/1.3) {
+                bigb1.style.opacity = '1';
+                bigb1.style.transform = 'translateX(0)';
 }
 });
 }
